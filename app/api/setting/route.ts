ce1db49bef19bb19fbf0f66250859d16cebe1c33
vexdou <yous01888@gmail.com>
2026-08-19 11:39:35 +0300

    Update route.ts

--- app/api/setting/route.ts
+++ app/api/setting/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
-import clientPromise from "@/lib/mongodb";
+import clientPromise from "../../../lib/mongodb";
+import clientPromise from "../../../lib/mongodb";
 
 const defaultSettings = {
   profile: {
